eb5d87992d405c1d5336524cbac5216decd960d7
Mohamed jinas <boringdragon98@gmail.com>
2023-05-16 03:32:38 +0500

    require blade heroicons inside the package & icon name to latest version

--- resources/views/components/empty.blade.php
+++ resources/views/components/empty.blade.php
@@ -1,6 +1,6 @@
 <div class="bg-slate-400/50 rounded text-center place-content-center py-40">
     <span>
-    <x-heroicon-o-clipboard-list class="text-slate-600 mx-auto h-20 w-20" />
+    <x-heroicon-o-clipboard-document-list class="text-slate-600 mx-auto h-20 w-20" />
     </span>
 
     <p class="text-slate-600 pt-8 font-medium">No Items Found</p>
