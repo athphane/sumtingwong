7c79ef27bc164e79acbc0c2a06183403f4b2a4cb
Athfan Khaleel <athphane@gmail.com>
2023-05-16 00:13:49 +0500

    - update migrations and models

---
+++ src/Models/SumtingwongRecord.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Athphane\Sumtingwong\Models;
+
+use Athphane\Sumtingwong\Database\Factories\SumtingwongRecordFactory;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Foundation\Auth\User;
+
+class SumtingwongRecord extends Model
+{
+    use HasFactory;
+
+    protected $table = 'sumtingwong_entries';
+
+    protected $casts = [
+        'addressed_at' => 'datetime',
+    ];
+
+    protected static function newFactory()
+    {
+        return new(SumtingwongRecordFactory::class);
+    }
+
+    public static function latest(): Builder
+    {
+        return static::query()->latest();
+    }
+
+    public static function latestEntry(): self
+    {
+        return static::query()->latest()->first();
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getFormattedUserAttribute(): ?string
+    {
+        if ($this->user) {
+            return "{$this->user?->name} ({$this->user_id})";
+        }
+
+        return null;
+    }
+
+
+}
