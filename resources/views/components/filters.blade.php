c7c52daad6ea138c3996243a44b9ebc4fbccdc49
Athfan Khaleel <athphane@gmail.com>
2023-05-16 23:04:36 +0500

    - restyle select input for uniformity

--- resources/views/components/filters.blade.php
+++ resources/views/components/filters.blade.php
@@ -1,29 +1,37 @@
-<div class="my-3">
+<div class="mb-3">
     <form class="flex items-center justify-between" action="{{ route('sumtingwongs.index') }}" method="GET">
 
         <div class="flex items-end gap-5">
             <div>
-                <label for="orderBy" class="block text-sm font-medium leading-6 text-gray-900">Order By</label>
-                <div class="mt-2">
+                <label for="orderBy" class="block mb-2 text-sm font-medium text-gray-900">
+                    {{ __('Order By') }}
+                </label>
+                <select id="orderBy" name="orderBy"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg
+                        focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    <option value="">{{ __('No options selected') }}</option>
+                    <option value="severity"
+                            @selected(Request::get('orderBy') === 'severity')
+                    >{{ __('Severity') }}</option>
+                </select>
+            </div>
 
-                  <select name="orderBy" class="block w-full rounded-md border-0 py-2.5 text-gray-900 shadow-sm text-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600">
-                    <option value="">No options selected</option>
-                    <option value="severity">Severity</option>
-                  </select>
-                </div>
-              </div>
-
-              <div>
-                <button class="block rounded-lg bg-indigo-600 px-5 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring">Filter</button>
-              </div>
-
+            <button
+                class="block rounded-lg bg-indigo-600 px-5 py-2.5 text-sm font-medium text-white transition
+                hover:bg-indigo-700 focus:outline-none focus:ring">
+                {{ __('Filter') }}
+            </button>
         </div>
 
-      @if(Request::has('orderBy'))
-      <div>
-        <a href="{{ route('sumtingwongs.index') }}" class="block rounded-lg hover:text-white bg-transparent border border-indigo-600 px-5 py-3 text-sm font-medium text-gray-800 transition hover:bg-indigo-700 focus:outline-none focus:ring">Clear Filters</a>
-      </div>
-      @endif
+        @if(Request::has('orderBy'))
+            <div>
+                <a href="{{ route('sumtingwongs.index') }}"
+                   class="block rounded-lg hover:text-white bg-transparent border border-indigo-600 px-5 py-2.5 mt-6
+                   text-sm font-medium text-gray-800 transition hover:bg-indigo-700 focus:outline-none focus:ring">
+                    {{ __('Clear Filters') }}
+                </a>
+            </div>
+        @endif
 
     </form>
 </div>
