4c4b3a4a0a5191cd5afc2eda6054f1a4d3a48c0f
Athfan Khaleel <athphane@gmail.com>
2023-05-18 17:37:36 +0500

    - show flashing icon on table only if not addressed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -33,7 +33,7 @@
                 @foreach($sumtingwongs as $sumtingwong)
                     <tr>
                         <td>
-                            @if($sumtingwong->severity === 'high')
+                            @if($sumtingwong->addressed_at == null && $sumtingwong->severity === 'high')
                                 <span class="relative flex h-3 w-3 ml-3">
                                     <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
                                     <span class="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
